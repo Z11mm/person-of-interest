273606b58c32821bf0af5b3e9f323601e7111d1f
Ghad <zimobie@gmail.com>
2020-04-20 17:26:15 +0100

    adding styles to navigation using styled components

--- src/components/navigation/NavigationStyles.jsx
+++ src/components/navigation/NavigationStyles.jsx
@@ -1,8 +1,21 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const CustomNavigation = styled.nav`
+const navigationStyles = css`
   display: flex;
   justify-content: flex-end;
+  transition: color 0.1s;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.5;
+  }
+`;
+
+const CustomNavigation = styled.nav`
+  text-decoration: underline;
+  padding: 0 1rem;
+
+  ${navigationStyles}
 `;
 
 export default CustomNavigation;
