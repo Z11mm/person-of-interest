74dbe8fd1de5c7946c985cd075ee98a65eea987d
Ghad <zimobie@gmail.com>
2020-04-22 15:09:44 +0100

    installed particle js and applied animation to background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
@@ -6,9 +7,30 @@ import ImageInputForm from './components/image-input-form/ImageInputForm';
 import Rank from './components/rank/Rank';
 import './App.css';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 180,
+      density: {
+        enable: 'true',
+        value_area: 789,
+      },
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: 'true',
+        mode: 'repulse',
+      }
+    },
+  },
+};
+
 function App() {
   return (
     <div className='App'>
+      <Particles className='particles' params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
