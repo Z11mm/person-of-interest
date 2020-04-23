b41f7dd6886d6ac870c5f85e388bbad81ab7870c
Ghad <zimobie@gmail.com>
2020-04-23 18:28:02 +0100

    installed and set up clarifai api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageInputForm from './components/image-input-form/ImageInputForm';
 import Rank from './components/rank/Rank';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '88005c5ed5844cbeadf1dcd9c209611b',
+});
 
 const particlesOptions = {
   particles: {
@@ -42,6 +47,19 @@ class App extends Component {
 
   handleSubmit = () => {
     console.log('click');
+    app.models
+      .predict(
+        'a403429f2ddf4b49b307e318f00e528b',
+        'https://samples.clarifai.com/face-det.jpg'
+      )
+      .then(
+        function (response) {
+          console.log(response);
+        },
+        function (err) {
+          // there was an error
+        }
+      );
   };
 
   render() {
