a741a59f8c30dda66e04eba46c52b3aa7894ca35
Z11mm <zimobiechina@gmail.com>
2020-05-13 10:17:18 +0100

    Add catch block to fetch api in App component

--- src/App.js
+++ src/App.js
@@ -113,7 +113,8 @@ class App extends Component {
             .then(response => response.json())
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count }));
-            });
+            })
+            .catch(err => console.log(err));
         }
         this.setBoundingBox(this.calculateFaceRegion(response));
       })
