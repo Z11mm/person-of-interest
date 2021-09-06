164f0ab50f6ab4e3fca09ac35e86327f5de7bbac
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-09-06 18:49:54 +0100

    Add build folder and htaccess config for apache

---
+++ build/precache-manifest.9d49edb169ed4c2d17828212a3c21e8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41bf759d89d719d3a8aed60b967434ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7c322e796f935683e1d",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0c581d7ccc76a6f9ef45",
+    "url": "/static/css/main.a27c62c9.chunk.css"
+  },
+  {
+    "revision": "e7c322e796f935683e1d",
+    "url": "/static/js/2.a96dc7aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a96dc7aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c581d7ccc76a6f9ef45",
+    "url": "/static/js/main.ae71ca22.chunk.js"
+  },
+  {
+    "revision": "6617e87708047d3ac1fd",
+    "url": "/static/js/runtime-main.64a97fc3.js"
+  }
+]);
